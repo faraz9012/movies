5cac86a1a9cdb7f3562dc80aa0504c983a0c24b8
faraz9012 <54522532+faraz9012@users.noreply.github.com>
2023-10-25 18:22:49 +0500

    [feat]: created layout and consumed IMDB test api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
+import { useState, useEffect } from 'react';
+import MovieCard  from './MovieCard';
+
 import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=3fee686b';
 
 const App = () => {
-  const name = 'John';
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  } 
+
+  useEffect(() => {
+    searchMovies('Superman');
+  }, []);
   return (
-    <div className="App">
-      <h1>Hello {name}!</h1>
+      <div className='App'>
+        <h1>Search Movies App on React</h1>
+
+        <div className='search'>
+          <input 
+            placeholder='Search for...'
+            value="Spiderman"
+            onChange={()=>{}}
+          />
+          <img
+            src={SearchIcon}
+            alt="search"
+            onClick={()=>{}}
+            />
+        </div>
+
+        {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
